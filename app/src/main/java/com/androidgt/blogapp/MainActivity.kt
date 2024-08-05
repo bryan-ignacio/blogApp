34a2c703ab7a579d09bd166b5414ad3462959d3b
Bryan <bryan-ignacio@outlook.com>
2024-08-04 21:53:27 -0600

    takephoto function added.

--- app/src/main/java/com/androidgt/blogapp/MainActivity.kt
+++ app/src/main/java/com/androidgt/blogapp/MainActivity.kt
@@ -1,78 +1,63 @@
 package com.androidgt.blogapp
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.firestore.FirebaseFirestore
 
+// FireBase Tomar Fotos.
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var imageView: ImageView
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // la consulta y ingreso de informacion a la base de datos es asincrona esto quiere decir que se ejecuta en segundo plano.
-        // y que una tarea puede terminar antes que la otra.
-        // get y set son metodos asincronos.
-        // Las interfaces addOnSuccessListener y OnFailureListener son asincronas.
+        val btnTakePicture = findViewById<Button>(R.id.btn_take_picture)
+        // la variable guarda la referencia del elemento xml.
+        imageView = findViewById(R.id.imageView)
 
-        //CONSULTAR INFORMACION.
-//  creamos la instancia de la base de datos.
-        val db = FirebaseFirestore.getInstance()
-//  obtenemos los datos de la base de datos.
-//        esta esta dentro de un collection que es document y dentro del documento obtenemos con get() busca solo una vez la informacion
-//        la data.
-        db.collection("ciudades").document("NY").get().addOnSuccessListener { document ->
-            document?.let {
-                // convierte el documento en un objeto de tipo ciudad.
-                // lo guardamos en la variable ciudad.
-                val ciudad = document.toObject(Ciudad::class.java)
-                Log.d("FireBase", "Color: ${ciudad?.color}")
-                Log.d("FireBase", "Population: ${ciudad?.population}")
-                Log.d("FireBase", "PostalCode: ${ciudad?.pc}")
-//                val color = document.getString("color")
-//                val population = document.getLong("population")
-//                Log.d("Firebase", " color: ${color}")
-//                Log.d("Firebase", " population: ${population}")
-            }
-        }.addOnFailureListener { exception ->
-            Log.e("FireBase", "Error getting documents: ${exception.toString()}")
+        btnTakePicture.setOnClickListener {
+            dispatchTakePictureIntent()
         }
+    }
 
-        //INGRESAR INFORMACION A LA BASE DE DATOS.
 
-        // se guarda en la misma coleccion ciudades.
-        // se crea un nuevo documento que se llama "NY"
-        // luego ingresamos los datos que queremos ingresar a la base de datos
-        db.collection("ciudades").document("LA").set(
-            // ingresando un objeto.
-            Ciudad("Red", 3000000)
-            //es un escucha si ha exito o no. en la obtension de la informacion.
-        ).addOnSuccessListener {
-            Log.d("Firebase", "Se guardo la ciudad correctamente.")
-        }.addOnFailureListener { exception ->
-            Log.e("Firebase", "Error getting documents: ${exception.toString()}")
+    // metodo que tomara la foto.
+    private fun dispatchTakePictureIntent() {
+        // Lo que hace intent encuentra cualquier app abre la camara con actionimageCapture.
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            // retorna un resultado de la foto tomada.
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No se encontro app para tomar foto", Toast.LENGTH_SHORT).show()
         }
+    }
 
+    //
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // compara los resultados.
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
-        // OBTENER INFORMACION EN TIEMPO REAL.
-        // cada vez que se actualice un valor de AZ devuelve la data con los ultimos cambios.
-        db.collection("ciudades").document("AZ").addSnapshotListener { snapshot, exception ->
-            snapshot?.let { document ->
-                val ciudad = document.toObject(Ciudad::class.java)
-                Log.d("FireBase", "Color: ${ciudad?.color}")
-                Log.d("FireBase", "Population: ${ciudad?.population}")
-                Log.d("FireBase", "PostalCode: ${ciudad?.pc}")
-            }
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+
+            // le asignamos el bitmap al imageView.
+            imageView.setImageBitmap(imageBitmap)
+
         }
     }
 }
-
-data class Ciudad(
-    val color: String = "",
-    val population: Long = 0,
-    val pc: Int = 0
-)
