1340529d1e120d7b15b800c084888161ce6aa97b
Bryan <bryan-ignacio@outlook.com>
2024-08-10 18:38:26 -0600

    navigation best form added.

--- app/src/main/java/com/androidgt/blogapp/MainActivity.kt
+++ app/src/main/java/com/androidgt/blogapp/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.androidgt.blogapp
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.graphics.Bitmap
+
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.androidgt.blogapp.core.hide
+import com.androidgt.blogapp.core.show
 import com.androidgt.blogapp.databinding.ActivityMainBinding
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.tasks.await
-import java.io.ByteArrayOutputStream
-import java.util.UUID
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+        observerDestinationChange()
+    }
+
+    private fun observerDestinationChange(){
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 
 }
