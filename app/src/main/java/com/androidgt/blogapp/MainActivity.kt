f67af1dbea84794a29e1799d7aebd242a6a04b3d
Bryan <bryan-ignacio@outlook.com>
2024-08-10 18:10:44 -0600

    menu navigation added.

--- app/src/main/java/com/androidgt/blogapp/MainActivity.kt
+++ app/src/main/java/com/androidgt/blogapp/MainActivity.kt
@@ -11,6 +11,10 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.androidgt.blogapp.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.tasks.await
@@ -18,9 +22,16 @@ import java.io.ByteArrayOutputStream
 import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
 }
---
+++ app/src/main/java/com/androidgt/blogapp/ui/camera/CameraFragment.kt
@@ -0,0 +1,13 @@
+package com.androidgt.blogapp.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.androidgt.blogapp.R
+
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+}
