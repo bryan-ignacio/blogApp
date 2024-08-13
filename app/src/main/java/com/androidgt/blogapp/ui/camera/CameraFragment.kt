6569ce5392feb7454560af76ab1971913d1797ff
Bryan <bryan-ignacio@outlook.com>
2024-08-12 18:40:29 -0600

    take photo and preview added.

--- app/src/main/java/com/androidgt/blogapp/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/androidgt/blogapp/ui/camera/CameraFragment.kt
@@ -17,23 +17,20 @@ import com.androidgt.blogapp.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
 
-
-    private val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var binding: FragmentCameraBinding
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private var bitmap: Bitmap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding = FragmentCameraBinding.bind(view)
-
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         } catch (e: ActivityNotFoundException) {
             Toast.makeText(
                 requireContext(),
-                "no se encontro app para abrir camara.",
+                "No se encontro app para abrir camara.",
                 Toast.LENGTH_SHORT
             ).show()
         }
@@ -43,7 +40,8 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.imgAddPhoto.setImageBitmap(imageBitmap)
+            binding.postImage.setImageBitmap(imageBitmap)
+            bitmap = imageBitmap
         }
     }
 }
